3e311a61df4690ae5e1918ad870dc4f9c2c49f99
thanhnhile <marrythanhnhipl205@gmail.com>
2022-09-30 16:58:22 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+//const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const morgan = require("morgan");
+
+//set path to .env file
+//dotenv.config({ path: './.env' });
+
+const app = express();
+//use to log
+app.use(morgan("combined"));
+
+//temp for connect DB
+const MONGO_USER = 'thanhnhile';
+const MONGO_PASS = 'bYdLPCvQVM4x5XVr';
+//Connect to MongoDB
+mongoose
+    .connect(
+        `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.r0veury.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        console.log("Connected to MongoDB");
+        startServer();
+    })
+    .catch((err) => console.error(err));
+
+//start server
+const startServer = () => {
+    // add this middleware to read post request body
+    app.use(express.json());
+    app.use(express.text());
+
+    /** Rules of our API */
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+
+        next();
+    });
+    /* Routes */
+    app.get('/', (req, res) => res.send('Hello World!'));
+
+    /** Error handling */
+    app.use((req, res, next) => {
+        const error = new Error("not found");
+        return res.status(404).json({
+            message: error.message,
+        });
+    });
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server is listening on port ${PORT}!`))
+}
